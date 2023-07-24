8ad1b4c12c368fca5365c70e4e15eef3d245524a
Arnold W <arnoldwwong@gmail.com>
2023-07-24 15:15:43 +1000

    Target button success

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
-const add = (a, b) => a + b;
+//const calculator = document.querySelector('#calc-container');
+const allButtons = document.querySelector("#all-buttons");
+//const numberButton = document.querySelectorAll('.number');
+//const operatorButton = calculator.querySelectorAll('.operator');
+//const allClear = document.querySelector('.all-clear');
+//const deleteButton = calculator.querySelector('.delete');
+//const equalsButton = document.querySelector('.equals');
+//const inputDisplay = calculator.querySelector('.input');
+//const resultDisplay = calculator.querySelector('.result');
 
-const subtract = (a, b) => a - b;
+allButtons.addEventListener('click', e => {
+    console.log('Button works!');
+});
 
-const multiply = (a, b) => a * b;
 
-const divide = (a, b) => a / b;
+//let a
+//let b
+//let c
+
+//const add = (a, b) => a + b;
+
+//const subtract = (a, b) => a - b;
+
+//const multiply = (a, b) => a * b;
+
+//const divide = (a, b) => a / b;
+
+//function input() {
+//    document.getElementByClass('input').value =
+//    document.getElementByClass('button').value;
+//}
